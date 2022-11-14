b0dd4933d54cfa7c64fd6e71cdff5ad90c38e075
sumitflyweis <node3@flyweis.technology>
2022-11-14 17:14:07 +0530

    make it better

--- models/User.js
+++ models/User.js
@@ -38,7 +38,7 @@ const userSchema = new mongoose.Schema(
 
     AboutMe: { type: String, trim: true },
 
-    User_Name: { type: String, unique: true, trim: true },
+    User_Name: { type: String, trim: true },
 
     User_Images: [{ type: String }],
 
