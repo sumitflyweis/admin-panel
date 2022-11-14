9edeafeb7fc38637afa79f5fb43453096b74e34f
sumitflyweis <node3@flyweis.technology>
2022-11-14 17:36:28 +0530

    make it better

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -242,68 +242,68 @@ module.exports.GetUserProfiles = async (req, res, next) => {
 
 //post for blog user
 
-module.exports.postuserBlogs = async (req, res) => {
-  let photo = req.body;
-  photo["blog_Images"] = [req.file.originalname];
-  let { Date, User_Name, sub_Title, Intro, blog_Images } = photo;
+// module.exports.postuserBlogs = async (req, res) => {
+//   let photo = req.body;
+//   photo["blog_Images"] = [req.file.originalname];
+//   let { Date, User_Name, sub_Title, Intro, blog_Images } = photo;
 
-  try {
-    if (!(Date && User_Name && sub_Title && Intro && blog_Images)) {
-      res
-        .status(400)
-        .json({ message: "All fields are required", status: false });
-    } else {
-      const getResponce = await blog.create({
-        User_Name,
-        Date,
-        sub_Title,
-        Intro,
-        blog_Images,
-      });
+//   try {
+//     if (!(Date && User_Name && sub_Title && Intro && blog_Images)) {
+//       res
+//         .status(400)
+//         .json({ message: "All fields are required", status: false });
+//     } else {
+//       const getResponce = await blog.create({
+//         User_Name,
+//         Date,
+//         sub_Title,
+//         Intro,
+//         blog_Images,
+//       });
 
-      if (!getResponce) {
-        res
-          .status(400)
-          .json({ message: "User Blogs  is not created", status: false });
-      } else {
-        res.status(200).json({
-          message: "User Blog is created successfully",
-          data: getResponce,
-          status: true,
-        });
-      }
-    }
-  } catch (error) {
-    res.status(400).json({ message: error.message, status: false });
-  }
-};
+//       if (!getResponce) {
+//         res
+//           .status(400)
+//           .json({ message: "User Blogs  is not created", status: false });
+//       } else {
+//         res.status(200).json({
+//           message: "User Blog is created successfully",
+//           data: getResponce,
+//           status: true,
+//         });
+//       }
+//     }
+//   } catch (error) {
+//     res.status(400).json({ message: error.message, status: false });
+//   }
+// };
 
-//update blog for user
+// //update blog for user
 
-module.exports.UpdateBlogs = async (req, res) => {
-  let photo = req.body;
-  photo["blog_Images"] = [req.file.originalname];
-  // console.log(photo);
-  let { Date, User_Name, sub_Title, Intro, blog_Images } = photo;
+// module.exports.UpdateBlogs = async (req, res) => {
+//   let photo = req.body;
+//   photo["blog_Images"] = [req.file.originalname];
+//   // console.log(photo);
+//   let { Date, User_Name, sub_Title, Intro, blog_Images } = photo;
 
-  if (!(Date && User_Name && sub_Title && Intro && blog_Images)) {
-    res.status(400).json({ message: "All fields are required", status: false });
-  } else {
-    const updatedBlogs = await blog.findByIdAndUpdate(
-      { _id: req.params.id },
-      {
-        User_Name,
-        Date,
-        sub_Title,
-        Intro,
-        blog_Images,
-      }
-    );
-    if (!updatedBlogs) {
-      res.send("Unable to update Blogs");
-    }
-    res.send(updatedBlogs);
-  }
-};
+//   if (!(Date && User_Name && sub_Title && Intro && blog_Images)) {
+//     res.status(400).json({ message: "All fields are required", status: false });
+//   } else {
+//     const updatedBlogs = await blog.findByIdAndUpdate(
+//       { _id: req.params.id },
+//       {
+//         User_Name,
+//         Date,
+//         sub_Title,
+//         Intro,
+//         blog_Images,
+//       }
+//     );
+//     if (!updatedBlogs) {
+//       res.send("Unable to update Blogs");
+//     }
+//     res.send(updatedBlogs);
+//   }
+// };
 
 //
