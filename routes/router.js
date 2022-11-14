bf9d3250877e086887ccdc1e527fabf6ef90184f
sumitflyweis <node3@flyweis.technology>
2022-11-14 15:48:10 +0530

    make it better

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.use("/auth", require("./auth.route"));
-
+router.use("/", require("./userRoutes"));
 router.use("/kundli", require("./userKundli.routes"));
 router.use("/chats", require("./chatRoutes"));
 router.use("/user", require("./userdetailsRoutes"));
@@ -12,5 +12,6 @@ router.use("/message", require("./message.routes"));
 router.use("/specification", require("./specificationRoutes"));
 router.use("/admin-auth", require("./admin"));
 router.use("/banner", require("./bannerRoutes"));
+router.use("/support", require("./supportRoute"));
 // router.use("/notification", require("./usersettingRoutes"));
 module.exports = router;
